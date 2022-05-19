4e36a49bbc9931b065116ae075c069aaccd806ab
franmartinv <95523823+franmartinv@users.noreply.github.com>
2022-05-19 12:34:14 +0200

    Update app_main.c

--- main/app_main.c
+++ main/app_main.c
@@ -35,25 +35,25 @@
 extern "C" {
 #endif
 
-#define		number_samples		5
-#define		MAX_DELAY			3600000		// 3600000 	milliseconds = 3600 seconds = 1 hour
-#define		MIN_DELAY			5000		// 5000		milliseconds = 5	seconds
+#define		number_samples			5
+#define		MAX_DELAY			3600000			// 3600000 	milliseconds = 3600 seconds = 1 hour
+#define		MIN_DELAY			5000			// 5000		milliseconds = 5	seconds
 #define		DELAY_SIZE			20
 #define		TIMEZONE			2			// timezone-zone for spain: UTC +2
 #define		YEAR_OFFSET			1900
-#define		MONTH_OFFSET		1
+#define		MONTH_OFFSET			1
 
-static 	const char 		*TAG 			= 		"MQTT_EXAMPLE";
-static	int				GLOBAL_DELAY	=		MIN_DELAY;
+static 	const char 		*TAG 		= 	"MQTT_EXAMPLE";
+static	int			GLOBAL_DELAY	=	MIN_DELAY;
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
-void 			hal_i2c_init();
+void 		hal_i2c_init();
 static void 	log_error_if_nonzero(const char *message, int error_code);
 static void 	mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data);
-void 			initialize_sntp();
-void 			obtain_time();
-void 			send_mqtt_time_data(esp_mqtt_client_handle_t client, int sec, int min, int hour, int day, int month, int year);
+void 		initialize_sntp();
+void 		obtain_time();
+void 		send_mqtt_time_data(esp_mqtt_client_handle_t client, int sec, int min, int hour, int day, int month, int year);
 
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
@@ -83,50 +83,50 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
 	esp_err_t		ret;
 
 	ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%d", base, event_id);
-    esp_mqtt_event_handle_t event = event_data;
-    esp_mqtt_client_handle_t client = event->client;
-    int msg_id;
+    	esp_mqtt_event_handle_t event = event_data;
+    	esp_mqtt_client_handle_t client = event->client;
+    	int msg_id;
 
-    switch ((esp_mqtt_event_id_t)event_id) {
-    case MQTT_EVENT_CONNECTED:
-        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+    	switch ((esp_mqtt_event_id_t)event_id) {
+    	case MQTT_EVENT_CONNECTED:
+        	ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
 
-        msg_id = esp_mqtt_client_subscribe(client, "ESP_control/run", 1);
-        ESP_LOGI(TAG, "sent subscribe to ESP_control successful!!, msg_id = %d", msg_id);
+        	msg_id = esp_mqtt_client_subscribe(client, "ESP_control/run", 1);
+        	ESP_LOGI(TAG, "sent subscribe to ESP_control successful!!, msg_id = %d", msg_id);
 
-        msg_id = esp_mqtt_client_subscribe(client, "ESP_control/delay", 1);
+        	msg_id = esp_mqtt_client_subscribe(client, "ESP_control/delay", 1);
 		ESP_LOGI(TAG, "sent subscribe to ESP_delay successful!!, msg_id = %d", msg_id);
         break;
-    case MQTT_EVENT_DISCONNECTED:
-        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+    	case MQTT_EVENT_DISCONNECTED:
+        	ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
         break;
 
-    case MQTT_EVENT_SUBSCRIBED:
-        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+   	case MQTT_EVENT_SUBSCRIBED:
+        	ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
         break;
-    case MQTT_EVENT_UNSUBSCRIBED:
-        ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+    	case MQTT_EVENT_UNSUBSCRIBED:
+        	ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
         break;
-    case MQTT_EVENT_PUBLISHED:
-        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+    	case MQTT_EVENT_PUBLISHED:
+        	ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
         break;
-    case MQTT_EVENT_DATA:
-        ESP_LOGI(TAG, "MQTT_EVENT_DATA");
-        printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
-        printf("DATA=%.*s\r\n", event->data_len, event->data);
+    	case MQTT_EVENT_DATA:
+		ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+		printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+		printf("DATA=%.*s\r\n", event->data_len, event->data);
 
-        for(i = 0; i <= event->topic_len; i++) {
-        	if(i == event->topic_len) read_topic[i] = '\0';
-        	else read_topic[i] = event->topic[i];
-        }
+		for(i = 0; i <= event->topic_len; i++) {
+			if(i == event->topic_len) read_topic[i] = '\0';
+			else read_topic[i] = event->topic[i];
+		}
 
-        for(i = 0; i <= event->data_len; i++) {
-        	if(i == event->data_len) read_data[i] = '\0';
-        	else read_data[i] = event->data[i];
-        }
+		for(i = 0; i <= event->data_len; i++) {
+			if(i == event->data_len) read_data[i] = '\0';
+			else read_data[i] = event->data[i];
+		}
 
-        if(strcmp(read_topic, "ESP_control/run") == 0) {
-        	if(strcmp(read_data, "STOP") == 0) {
+		if(strcmp(read_topic, "ESP_control/run") == 0) {
+			if(strcmp(read_data, "STOP") == 0) {
 				ret = esp_mqtt_client_publish(client, "ESP_control", "MQTT SERVICE STOPPED", 0, 1, 0);
 					if(ret == -1) {
 						printf("ERROR sending 'MQTT SERVICE STOPPED' to topic 'ESP_control'\n");
@@ -152,9 +152,9 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
 					}
 				ESP_LOGI(TAG, "MQTT SERVICE STARTED");
 			}
-        }
-        else if(strcmp(read_topic, "ESP_control/delay") == 0) {
-        	GLOBAL_DELAY = atoi(event->data);	//	convert char* to float variable
+        	}
+		else if(strcmp(read_topic, "ESP_control/delay") == 0) {
+			GLOBAL_DELAY = atoi(event->data);	//	convert char* to float variable
 
 			if(GLOBAL_DELAY > MAX_DELAY) {
 				GLOBAL_DELAY = MAX_DELAY;
@@ -164,24 +164,23 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
 			}
 
 			ESP_LOGI(TAG, "DELAY SELECTED: %d", GLOBAL_DELAY);
-        }
+		}
 
 
         break;
-    case MQTT_EVENT_ERROR:
-        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
-        if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
-            log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
-            log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
-            log_error_if_nonzero("captured as transport's socket errno",  event->error_handle->esp_transport_sock_errno);
-            ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
-
-        }
+    	case MQTT_EVENT_ERROR:
+        	ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+        	if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
+        	    	log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
+         		log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
+         	   	log_error_if_nonzero("captured as transport's socket errno",  event->error_handle->esp_transport_sock_errno);
+        	    	ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
+        	}
         break;
-    default:
-        ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+    	default:
+        	ESP_LOGI(TAG, "Other event id:%d", event->event_id);
         break;
-    }
+    	}
 }
 
 
@@ -287,21 +286,21 @@ void app_main(void)
 	char						*topic;
 	char						mqtt_data[100], strftime_buf[64];
 
-    ESP_LOGI(TAG, "[APP] Startup..");
-    ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
-    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
+	ESP_LOGI(TAG, "[APP] Startup..");
+	ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
+	ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
 
-    esp_log_level_set("*", ESP_LOG_INFO);
-    esp_log_level_set("MQTT_CLIENT", ESP_LOG_VERBOSE);
-    esp_log_level_set("MQTT_EXAMPLE", ESP_LOG_VERBOSE);
-    esp_log_level_set("TRANSPORT_BASE", ESP_LOG_VERBOSE);
-    esp_log_level_set("esp-tls", ESP_LOG_VERBOSE);
-    esp_log_level_set("TRANSPORT", ESP_LOG_VERBOSE);
-    esp_log_level_set("OUTBOX", ESP_LOG_VERBOSE);
+	esp_log_level_set("*", ESP_LOG_INFO);
+	esp_log_level_set("MQTT_CLIENT", ESP_LOG_VERBOSE);
+ 	esp_log_level_set("MQTT_EXAMPLE", ESP_LOG_VERBOSE);
+	esp_log_level_set("TRANSPORT_BASE", ESP_LOG_VERBOSE);
+	esp_log_level_set("esp-tls", ESP_LOG_VERBOSE);
+	esp_log_level_set("TRANSPORT", ESP_LOG_VERBOSE);
+	esp_log_level_set("OUTBOX", ESP_LOG_VERBOSE);
 
-    ESP_ERROR_CHECK(nvs_flash_init());
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
+	ESP_ERROR_CHECK(nvs_flash_init());
+	ESP_ERROR_CHECK(esp_netif_init());
+	ESP_ERROR_CHECK(esp_event_loop_create_default());
 
 	mode_number 	= 	1;
 
@@ -323,23 +322,23 @@ void app_main(void)
 
 
 
-    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
-     * Read "Establishing Wi-Fi or Ethernet Connection" section in
-     * examples/protocols/README.md for more information about this function.
-     */
-    ESP_ERROR_CHECK(example_connect());
+    	/* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+    	 * Read "Establishing Wi-Fi or Ethernet Connection" section in
+     	 * examples/protocols/README.md for more information about this function.
+     	*/
+    	ESP_ERROR_CHECK(example_connect());
 
     // SNTP configuring
 
-		time_t now;
-		struct tm timeinfo;
-		time(&now);
-		localtime_r(&now, &timeinfo);
+	time_t now;
+	struct tm timeinfo;
+	time(&now);
+	localtime_r(&now, &timeinfo);
 
-		sntp_servermode_dhcp(1);	// Set 1 if you want to request that the servers to be used for SNTP should be requested from the DHCP. Set 0 to not
-		initialize_sntp();			// Function to initialize sntp and obtain online time data from server
+	sntp_servermode_dhcp(1);	// Set 1 if you want to request that the servers to be used for SNTP should be requested from the DHCP. Set 0 to not
+	initialize_sntp();			// Function to initialize sntp and obtain online time data from server
 
-	// end SNTP configuring
+    // end SNTP configuring
 
     // Initialition of mqtt
 
@@ -471,7 +470,6 @@ void app_main(void)
 		//GLOBAL_DELAY = atof(event->data);
 
 		vTaskDelay(GLOBAL_DELAY/portTICK_RATE_MS);
-
     }
 }
 
